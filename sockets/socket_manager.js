ebd73a8a70dec8534ebda680d9f6b6b3b0f378a9
Tas-Hur <arthur.navarro@laposte.net>
2019-11-07 23:09:18 +0100

    forgot to add socket_manager.js

---
+++ sockets/socket_manager.js
@@ -0,0 +1,29 @@
+console.log("I am goods")
+
+var clients =[]
+
+var _init_manager = function(io){
+  io.on('connect', onRequest);
+}
+
+var onRequest = function(socket){
+  console.log('a user connected : ', socket.id);
+  clients.push(socket);
+  socket.emit("id", socket.id)
+  // setTimeout(disconnectSocket.bind(null,socket), 5000);
+}
+
+var disconnectSocket = function(socket){
+  console.log("a user leaves : ", socket.id)
+  // socket.disconnect(true);
+  // let index = clients.findIndex(client => client.id==socket.id)
+  // clients.splice(index, 1)
+  // return true;
+}
+
+module.exports=
+{
+  init:_init_manager,
+  sockets:clients,
+  disconnect:disconnectSocket,
+}
