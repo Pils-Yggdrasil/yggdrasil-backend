d1dfd26f4133c5801866a34a198ec26f3526d2ef
Tas-Hur <arthur.navarro@laposte.net>
2019-11-10 19:59:57 +0100

    achieve rendering force graph, struggle with text clipping

--- routes/index.js
+++ routes/index.js
@@ -40,16 +40,15 @@ router.get('/key_words', function(req, res, next) {
 router.get('/paper_id', function(req, res, next) {
   let paper_id = "0796f6cd7f0403a854d67d525e9b32af3b277331"
   let base_url = 'http://api.semanticscholar.org/v1/paper/'
-  let stub_url = base_url + paper_id;
-  let api_url = base_url+req.query.paper_id
+
   let socket_id = req.query.socket_id
-  references = []
   var sockets = require('../sockets/socket_manager.js').sockets
   console.log("# of connections : ",sockets.length)
   console.log("This socket is ",socket_id)
+
   var socket = sockets.find(sock => sock.id == socket_id)
-  console.log(socket.id)
-  requestPaper(stub_url)
+
+  requestPaper(base_url+paper_id)
   .then(doc=>{
     doc=doc.body
     res.json(
@@ -57,27 +56,46 @@ router.get('/paper_id', function(req, res, next) {
         doc
       ]
     );
-    let counter = 1;
-    console.log("# of references : ",doc.references.filter(ref=> ref.isInfluential).length)
-    console.log("# of citations : ",doc.citations.filter(ref=> ref.isInfluential).length)
-    doc.citations.filter(ref=>ref.isInfluential).forEach(ref=>{
+    let counter = 0;
+    var citations = doc.citations.filter(ref=>ref.isInfluential)
+    var references = doc.references.filter(ref=> ref.isInfluential)
+    console.log("# of citations : ",citations.length)
+    console.log("# of references : ",references.length)
+    citations.forEach(ref=>{
       requestPaper(base_url+ref.paperId)
       .then(doc=>{
-        console.log(counter)
-        console.log(doc.body.citations.length)
-        counter+=1;
         socket.emit('new_node', doc.body);
+        references.push(doc.body)
       })
       .catch(err=>{
-        // console.log(err)
+        console.log(err.message)
+      })
+      .finally(msg => {
+        console.log(counter)
+        counter+=1
+        if(counter == 90){
+          console.log("counter : ",counter)
+          socket.emit('done');
+        }
       })
     })
-    doc.references.filter(ref=>ref.isInfluential).forEach(ref=>{
+
+    references.forEach(ref=>{
       requestPaper(base_url+ref.paperId)
       .then(doc=>{
         socket.emit('new_node', doc.body);
+        references.push(doc.body)
       })
       .catch(err=>{
+        console.log(err.message)
+      })
+      .finally(msg => {
+        console.log(counter)
+        counter+=1
+        if(counter == 80){
+          console.log("counter : ",counter)
+          socket.emit('done');
+        }
       })
     })
   })
