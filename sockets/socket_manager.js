2e26e0136beb6ba07b415f3a3c587ae9d008db2d
Oscar <oscar@oscar-Ubuntu-18.04>
2019-11-21 16:21:13 +0100

    fix socket behavious : now dies when user leaves

--- sockets/socket_manager.js
+++ sockets/socket_manager.js
@@ -9,8 +9,9 @@ var _init_manager = function(io){
 var onRequest = function(socket){
   console.log('a user connected : ', socket.id);
   clients.push(socket);
-  socket.emit("id", socket.id)
+  //socket.emit("id", socket.id)
   setTimeout(disconnectSocket.bind(null,socket), 120000);
+  socket.on('userLeave', userLeave.bind(null,socket));
 }
 
 var disconnectSocket = function(socket){
@@ -20,6 +21,16 @@ var disconnectSocket = function(socket){
   clients.splice(index, 1)
   return true;
 }
+
+
+
+var userLeave = function(socket){
+  // console.log("a user WANTS to leave : ", socket_id);
+  // let socket = clients.find(c => c.id == socket_id)
+  // console.log(socket)
+  disconnectSocket(socket)
+}
+
 
 module.exports=
 {
