540659b608d6ddb72674010580ecc75862939019
Tas-Hur <arthur.navarro@laposte.net>
2019-11-25 21:14:52 +0100

    merge master

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,18 @@ var request = require('request-promise');
 router.get('/', function(req, res, next) {
   console.log("root")
   res.json(
-    [
-      {
-        keyTest: "valueTest"
-      }
-    ]
+    [{
+      keyTest: "valueTest"
+    }]
   );
 });
 
 router.get('/author', function(req, res, next) {
   console.log("author lookup")
   res.json(
-    [
-      {
-        keyTest: "author"
-      }
-    ]
+    [{
+      keyTest: "author"
+    }]
   );
 })
 
@@ -36,58 +32,62 @@ router.get('/key_words', function(req, res, next) {
     var url_sem = 'http://api.semanticscholar.org/v1/paper/'
     //var key_words = req.query.key_words.replace(/ /g, "+");
     var key_words = "Gradient based learning";
-    url_cross = url_cross+key_words+"&facet=publisher-name:10&rows=25&sort=relevance&order=desc"
-    res.json({success:true})
+    url_cross = url_cross + key_words + "&facet=publisher-name:10&rows=25&sort=relevance&order=desc"
+    res.json({
+      success: true
+    })
 
     requestPaper(url_cross)
-    .then(list => {
-      list = list.body.message.items;
-      list.forEach(paper => {
-        requestPaper(url_sem+paper.DOI)
-        .then(pap => {
-          pap = pap.body;
-          console.log("TITLE = "+pap.title)
-          pap.cdpScore = computeCpDScore(pap, 2)
-          socket.emit('new_node', pap);
-          pap.citations.forEach(citation => {
-            requestPaper(url_sem+citation.paperId)
-            .then(cit => {
-              cit = cit.body;
-              console.log("CIT = "+cit.title)
-              cit.cdpScore = computeCpDScore(cit, 2)
-              socket.emit('new_node', cit);
-            })
-            .catch(err => {
-              console.log(err.message)
-            })
-          })
-          pap.references.forEach(reference => {
-            requestPaper(url_sem+reference.paperId)
-            .then(ref => {
-              ref = ref.body;
-              console.log("REF = "+ref.title)
-              ref.cdpScore = computeCpDScore(ref, 2)
-              socket.emit('new_node', ref);
-            })
-            .catch(err => {
-              console.log(err.message)
-            })
-          })
+      .then(list => {
+        list = list.body.message.items;
+        list.forEach(paper => {
+          requestPaper(url_sem + paper.DOI)
+            .then(pap => {
+              pap = pap.body;
+              console.log("TITLE = " + pap.title)
+              pap.cdpScore = computeCpDScore(pap, 2)
+              socket.emit('new_node', pap);
+              pap.citations.forEach(citation => {
+                requestPaper(url_sem + citation.paperId)
+                  .then(cit => {
+                    cit = cit.body;
+                    console.log("CIT = " + cit.title)
+                    cit.cdpScore = computeCpDScore(cit, 2)
+                    socket.emit('new_node', cit);
+                  })
+                  .catch(err => {
+                    console.log(err.message)
+                  })
+              })
+              pap.references.forEach(reference => {
+                requestPaper(url_sem + reference.paperId)
+                  .then(ref => {
+                    ref = ref.body;
+                    console.log("REF = " + ref.title)
+                    ref.cdpScore = computeCpDScore(ref, 2)
+                    socket.emit('new_node', ref);
+                  })
+                  .catch(err => {
+                    console.log(err.message)
+                  })
+              })
 
-        })
-        .catch(err => {
-          console.log(err.message)
+            })
+            .catch(err => {
+              console.log(err.message)
+            })
         })
       })
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-    .finally()
+      .catch(err => {
+        console.log(err.message)
+      })
+      .finally()
 
   } catch (err) {
     console.log(err.message)
-    res.json({error : err.message})
+    res.json({
+      error: err.message
+    })
   }
 })
 
@@ -107,102 +107,106 @@ router.get('/paper_id', function(req, res, next) {
   var param_exponent_cit = 2;
   var param_exponent_ref = 2;
   var sockets = require('../sockets/socket_manager.js').sockets
-  console.log("# of connections : ",sockets.length)
-  console.log("This socket is ",socket_id)
+  console.log("# of connections : ", sockets.length)
+  console.log("This socket is ", socket_id)
 
   var socket = sockets.find(sock => sock.id == socket_id)
 
-  requestPaper(base_url+paper_id)
-  .then(doc=>{
-    doc=doc.body
-    doc.cdpScore = computeCpDScore(doc, param_exponent_cit)
-    doc.topics.forEach(top => {
-      paper_topics.push(top.topicId)
+  requestPaper(base_url + paper_id)
+    .then(doc => {
+      doc = doc.body
+      doc.cdpScore = computeCpDScore(doc, param_exponent_cit)
+      doc.topics.forEach(top => {
+        paper_topics.push(top.topicId)
+      })
+      res.json({
+        success: true
+      })
+      socket.emit('new_node', doc);
+      let counter = 1;
+      var citations = doc.citations.filter(ref => ref.isInfluential)
+      var references = doc.references.filter(ref => ref.isInfluential)
+      console.log(citations[0])
+      citations.forEach(ref => {
+        requestPaper(base_url + ref.paperId)
+          .then(doc => {
+            citation_topics = [];
+            // console.log(doc.body.citations.length)
+            doc.body.topics.forEach(top => {
+              citation_topics.push(top.topicId)
+            })
+            var is_in = 0;
+            citation_topics.forEach(top => {
+              if (paper_topics.includes(top)) {
+                is_in += 1;
+              }
+            })
+            doc.body.cdpScore = computeCpDScore(doc.body, param_exponent_cit)
+            if (is_in >= param_topic_cit) {
+              socket.emit('new_node', doc.body);
+            } else {
+              waiting_papers.push(doc.body)
+            }
+
+          })
+          .catch(err => {
+            console.log(err.message)
+          })
+          .finally(() => {
+            counter += 1;
+            console.log(counter)
+            if (counter == 500) {
+              socket.emit('done')
+            }
+          })
+      })
+      references.forEach(ref => {
+        requestPaper(base_url + ref.paperId)
+          .then(doc => {
+            ref_topics = [];
+            doc.body.topics.forEach(top => {
+              ref_topics.push(top.topicId);
+            })
+            var is_in = 0;
+
+            ref_topics.forEach(top => {
+              if (paper_topics.includes(top)) {
+                is_in += 1
+              }
+            })
+            if (is_in >= param_topic_ref) {
+              socket.emit('new_node', doc.body);
+            } else {
+              waiting_papers.push(doc.body)
+            }
+
+          })
+          .catch(err => {
+            console.log(err.message)
+          })
+
+          .finally(() => {
+            counter += 1;
+            console.log(counter)
+            if (counter == references.length - 1) {
+              socket.emit('done')
+            }
+          })
+      })
+
+
     })
-    res.json({success:true})
-    socket.emit('new_node', doc);
-    let counter = 1;
-    var citations = doc.citations.filter(ref=>ref.isInfluential)
-    var references = doc.references.filter(ref=>ref.isInfluential)
-    console.log(citations[0])
-    citations.forEach(ref=>{
-      requestPaper(base_url+ref.paperId)
-      .then(doc=>{
-        citation_topics = [];
-        // console.log(doc.body.citations.length)
-        doc.body.topics.forEach(top => {
-          citation_topics.push(top.topicId)
-        })
-        var is_in = 0;
-        citation_topics.forEach(top => {
-          if(paper_topics.includes(top)) {
-            is_in += 1;
-          }
-        })
-        doc.body.cdpScore = computeCpDScore(doc.body, param_exponent_cit)
-        if(is_in >= param_topic_cit){
-          socket.emit('new_node', doc.body);
-        } else {
-          waiting_papers.push(doc.body)
-        }
-
-      })
-      .catch(err=>{
-        console.log(err.message)
-      })
-      .finally(()=>{
-        counter+=1;
-        console.log(counter)
-        if(counter == 500){
-          socket.emit('done')
-        }
+    .catch(error => {
+      res.json({
+        error: error
       })
     })
-    references.forEach(ref=>{
-      requestPaper(base_url+ref.paperId)
-      .then(doc=>{
-        ref_topics = [];
-        doc.body.topics.forEach(top => {
-          ref_topics.push(top.topicId);
-        })
-        var is_in = 0;
-
-        ref_topics.forEach(top => {
-          if(paper_topics.includes(top)) {
-            is_in += 1
-          }
-        })
-        if(is_in >= param_topic_ref) {
-          socket.emit('new_node', doc.body);
-        } else {
-          waiting_papers.push(doc.body)
-        }
-
-      })
-      .catch(err=>{
-        console.log(err.message)
-      })
-
-      .finally(()=>{
-        counter+=1;
-        console.log(counter)
-        if(counter == references.length - 1){
-          socket.emit('done')
-        }
-      })
-    })
-
-
-  })
-  .catch(error=>{
-    res.json({error: error})
-  })
 })
 
-testTopics = function(doc, topics_full, topics){
+testTopics = function(doc, topics_full, topics) {
   // SOMETHING USED fOR TESTING
-  doc.topics.forEach(example=>{
-    if(!(topics.includes(example.topicId))){
+  doc.topics.forEach(example => {
+    if (!(topics.includes(example.topicId))) {
       topics.push(example.topicId);
     }
     topics_full.push(example.topicId);
@@ -211,25 +215,25 @@ testTopics = function(doc, topics_full, topics){
   console.log("---------->    La famille : " + topics.length)
 }
 
-computeCpDScore= function(doc, exponent){
+computeCpDScore = function(doc, exponent) {
   let age = new Date().getFullYear() - doc.year;
-  return doc.citations.length / Math.pow(age+1, 1/exponent)
+  return doc.citations.length / Math.pow(age + 1, 1 / exponent)
 }
 
-requestPaper = function(url){
+requestPaper = function(url) {
   var options = {
     uri: url,
     resolveWithFullResponse: true,
     json: true // Automatically parses the JSON string in the response
   }
-  return new Promise((resolve,reject)=>{
+  return new Promise((resolve, reject) => {
     request(options)
-    .then(res=>{
-      resolve(res)
-    })
-    .catch(err=>{
-      reject(err)
-    })
+      .then(res => {
+        resolve(res)
+      })
+      .catch(err => {
+        reject(err)
+      })
   })
 }
 
